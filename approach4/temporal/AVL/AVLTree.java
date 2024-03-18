953cf834210c9a512fd29d39be3d41b2bbe5ca21
leno-aph508 <aresparvizi@gmail.com>
2024-03-18 01:07:56 -0300

    feat: add AVL code.

--- approach4/temporal/AVL/AVLTree.java
+++ approach4/temporal/AVL/AVLTree.java
@@ -1,4 +1,29 @@
 package approach4.temporal.AVL;
 
-public class AVLTree {
+import approach4.IRowDetails;
+import approach4.temporal.skipList.Tower;
+import approach4.temporal.skipList.ToweredTypeUtils;
+
+import java.util.Random;
+
+public class AVLTree<KVER extends Comparable<KVER>,K extends Comparable<K>,V extends IRowDetails<K,V,KVER>> {
+    private final int partitionCapacity;
+    private KVER currentVersion;
+    //    private final double iterationProbability;
+    // An instance of the random number generator.
+    // TODO remove seed
+    public static Random random = new Random();
+
+    // no tail, as tail will be always null
+    private final Node<KVER,K,V> head;
+
+    //    private KVER currentVersion;
+    AVLTree(KVER initVersion, int partitionCapacity) throws Exception{
+        this.currentVersion = initVersion;
+        this.partitionCapacity = partitionCapacity;
+        this.head = new Node<>(initVersion, null)
+    }
+    insert(V row){
+
+    }
 }
--- approach4/temporal/AVL/Node.java
+++ approach4/temporal/AVL/Node.java
@@ -1,4 +1,46 @@
 package approach4.temporal.AVL;
 
-public class Node {
+import approach4.IRowDetails;
+import approach4.temporal.skipList.Tower;
+import approach4.temporal.temporalPartitions.Partitions;
+
+import java.util.ArrayList;
+
+public class Node<KVER extends Comparable<KVER>,K extends Comparable<K>, V extends IRowDetails<K, V, KVER>> {
+    public Node leftChild, rightChild, parent;
+
+    public int height;
+
+    protected byte[] digest;
+
+    public final K key;
+
+    public Partitions<K,V,KVER> value;
+
+    public final int partitionCapacity;
+
+    public Node(KVER version, V row) throws Exception {
+        this(version, row, 1);
+    }
+    public Node(KVER version, V row, int partitionCapacity) throws Exception {
+
+        this.key = row.getKey();
+
+        this.partitionCapacity = partitionCapacity;
+        value = new Partitions<>(this.partitionCapacity);
+        if (row != null) {
+            value.add(version, row);
+        }
+
+        digest = null;
+        leftChild = null;
+        rightChild = null;
+        parent = null;
+        height = 1;
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node();
+        System.out.println();
+    }
 }
